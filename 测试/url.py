686a5ac0b66ced837898893c430351878bea77e4
yuanyaunxuezhang <397910360@qq.com>
2021-10-27 23:55:10 +0800

    post 方法传值

--- 测试/url.py
+++ 测试/url.py
@@ -26,5 +26,30 @@ ssl._create_default_https_context = ssl._create_unverified_context
 #     print('time out!')
 
 
-response = urllib.request.urlopen("http://httpbin.org/get")
-print(response.status)
+# response = urllib.request.urlopen("http://httpbin.org/get")
+# print(response.status)
+
+
+# response = urllib.request.urlopen("http://www.baidu.com")
+# print(response.getheaders()) #获取全部的头部信息
+# print()
+# print(response.getheader("Bdqid")) #获取其中一个值
+
+
+
+
+
+
+#爬取的时候不然服务器知道自己是爬虫
+# url = "https://www.douban.com"
+url = "http://httpbin.org/post"
+data = bytes(urllib.parse.urlencode({"hello":'ok'}),encoding="utf-8")
+headers = {
+"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36 Edg/95.0.1020.30'
+}
+req = urllib.request.Request(url=url,data=data,headers=headers,method="POST")
+response = urllib.request.urlopen(req)
+print(response.read().decode('utf-8'))
+
+
+
