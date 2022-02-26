1fee5b3a53dee874c8ca6335c31eea5f5197f34f
yuanyaunxuezhang <397910360@qq.com>
2022-02-26 09:56:40 +0800

    部分修改

--- 爬虫/demo001/武昌首义学院.py
+++ 爬虫/demo001/武昌首义学院.py
@@ -2,7 +2,7 @@
 # 时          间 : 2022/2/6 22:13
 # 高贵的高级工程师 : 元元学长
 import requests
-
+import time
 url = "http://www.wsyu.edu.cn/"
 headers = {
 "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Mobile Safari/537.36"
@@ -10,14 +10,15 @@ headers = {
 # resp = requests.get(url)
 # resp.encoding = "utf-8"
 i = 0
-while(i<1000000):
+while(True):
     resp = requests.get(url,headers=headers)
     resp.encoding = "utf-8"
-    resp.close()
-    print(resp.close())
+    # resp.close()
+    time.sleep(1)
+    # print(resp.close())
     i = i+1
     print("次数",end='\t')
-    print(i,end='\t')
+    print(str(i)+ '\t' + str(resp),end='\t')
     print()
 
 print("访客增加一百万成功")
