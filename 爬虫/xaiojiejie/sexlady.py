1342c6f506c190c3183c54fedbc27d3e84f5de76
yuanyaunxuezhang <397910360@qq.com>
2022-02-26 09:56:05 +0800

    不同api图片获取

---
+++ 爬虫/xaiojiejie/sexlady.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# 时          间 : 2022/2/25 19:34
+# 高贵的高级工程师 : 元元学长
+import requests
+import time
+
+
+url = "https://api.lyiqk.cn/sexylady"
+headers = {
+"User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Mobile Safari/537.36"
+}
+num = 135
+
+
+
+while(True):
+    resp = requests.get(url, headers=headers)
+    img_name = str(num)+".jpg"     #给写入的文件加入拓展名
+    num = num + 1
+    # print(resp.text)
+    with open(r"sexlady/"+img_name,mode="wb") as f:
+        f.write(resp.content)   #图片内容写入到文件
+        time.sleep(1)       #sleep里面的参数单位为秒
+        print("第"+str(num)+"次下载")
---
+++ 爬虫/xaiojiejie/小姐姐url.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# 时          间 : 2022/2/25 12:45
+# 高贵的高级工程师 : 元元学长
+
+import requests
+import time
+import re
+
+url = "https://imgapi.cn/cos.php"
+headers = {
+"User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Mobile Safari/537.36"
+}
+num = 1
+
+
+
+while(True):
+    resp = requests.get(url, headers=headers)
+    img_name = str(num)+".jpg"     #给写入的文件加入拓展名
+    num = num + 1
+    # print(resp.text)
+    with open(r"dongman/"+img_name,mode="wb") as f:
+        f.write(resp.content)   #图片内容写入到文件
+        time.sleep(1)       #sleep里面的参数单位为秒
+        print("第"+str(num)+"次下载")
